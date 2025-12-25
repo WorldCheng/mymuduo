afc398b466e4f6d15d0d2bc2a00183f0d9579ee5
WorldCheng <1913711216@qq.com>
2025-12-25 20:35:56 +0800

    1

--- EpollPoller.cpp
+++ EpollPoller.cpp
@@ -4,9 +4,9 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 
-const int kNew = -1;
-const int kAdded = 1;
-const int kDeleted = 2;
+const int kNew = -1;  // 表示Channel是新创建的，还未添加到epoll中
+const int kAdded = 1; // 表示Channel已经添加到epoll中进行监听
+const int kDeleted = 2; // 表示Channel已经从epoll中删除，但对象仍然存在
 
 EpollPoller::EpollPoller(EventLoop *loop)
     : Poller(loop), epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
@@ -20,3 +20,7 @@ EpollPoller::~EpollPoller() { ::close(epollfd_); }
 Timestamp EpollPoller::poll(int timeoutMs, ChannelList *activeChannels) {}
 void EpollPoller::removeChannel(Channel *channel) {}
 void EpollPoller::updateChannel(Channel *channel) {}
+
+void EpollPoller::fillActiveChannels(int numEvents,
+                                     ChannelList *activeChannels) const {}
+void EpollPoller::update(int operation, Channel *channel) {}
